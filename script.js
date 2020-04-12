926eb9dbdb2eff6e8c5647bdb43b25921d84c086
Yago Ananias <yago_souza92@yahoo.com.br>
2020-04-12 18:22:40 -0300

    Expressão Regular Email

--- script.js
+++ script.js
@@ -18,30 +18,38 @@ function mostraSucesso(input) {
     controleFormulario.className = 'controle-formulario sucesso';
 }
 
+//Email é Válido? Test retorna true se o email contiver valor de re
+function emailValido(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
 // Listeners
 formulario.addEventListener('submit', function(e) {
     e.preventDefault();
 
     if(usuario.value === '') {
-        mostraErro(usuario, 'Usuário não válido');
+        mostraErro(usuario, 'Campo Obrigatório');
     } else {
         mostraSucesso(usuario);
     }
 
     if(email.value === '') {
-        mostraErro(email, 'Email não válido');
+        mostraErro(email, 'Campo Obrigatório');
+    } else if(!emailValido(email.value, 'Email Inválido')) {
+        mostraErro(email, 'Email incorreto')
     } else {
         mostraSucesso(email);
     }
 
     if(senha.value === '') {
-        mostraErro(senha, 'Senha não válida');
+        mostraErro(senha, 'Campo Obrigatório');
     } else {
         mostraSucesso(senha);
     }
 
     if(senha2.value === '') {
-        mostraErro(senha2, 'Senha não válida');
+        mostraErro(senha2, 'Campo Obrigatório');
     } else {
         mostraSucesso(senha2);
     }        
